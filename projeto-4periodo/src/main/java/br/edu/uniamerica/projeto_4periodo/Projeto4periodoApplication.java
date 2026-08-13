b685b239bdc4b06ced071292b9a9b48b081e68c9
Adriano Ramos dos Santos Junior <adriannoordsj@gmail.com>
2026-08-13 17:47:27 -0300

    Iniciando projeto spring

---
+++ projeto-4periodo/src/main/java/br/edu/uniamerica/projeto_4periodo/Projeto4periodoApplication.java
@@ -0,0 +1,13 @@
+package br.edu.uniamerica.projeto_4periodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projeto4periodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projeto4periodoApplication.class, args);
+	}
+
+}
