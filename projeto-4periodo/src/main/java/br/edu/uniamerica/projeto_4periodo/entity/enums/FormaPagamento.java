15090776a83941c09458d28821131172d7cfafbd
Adriano Ramos dos Santos Junior <adriannoordsj@gmail.com>
2026-08-13 23:42:17 -0300

    packages, falta as classes

---
+++ projeto-4periodo/src/main/java/br/edu/uniamerica/projeto_4periodo/entity/enums/FormaPagamento.java
@@ -0,0 +1,7 @@
+package br.edu.uniamerica.projeto_4periodo.entity.enums;
+
+public enum FormaPagamento {
+    PIX,
+    CARTAO,
+    DINHEIRO
+}
---
+++ projeto-4periodo/src/main/java/br/edu/uniamerica/projeto_4periodo/entity/enums/TipoMovimentacao.java
@@ -0,0 +1,6 @@
+package br.edu.uniamerica.projeto_4periodo.entity.enums;
+
+public enum TipoMovimentacao {
+    ENTRADA,
+    SAIDA
+}
