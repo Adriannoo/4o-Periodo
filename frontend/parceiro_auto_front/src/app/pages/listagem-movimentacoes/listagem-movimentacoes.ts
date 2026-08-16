80ff64e2a5ead423647fb6cce70c15a77162ae98
mateus <teushdsouza@gmail.com>
2026-08-15 23:14:24 -0300

    feat: register page

--- frontend/parceiro_auto_front/src/app/pages/listagem-movimentacoes/listagem-movimentacoes.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-listagem-movimentacoes',
-  imports: [],
-  templateUrl: './listagem-movimentacoes.html',
-  styleUrl: './listagem-movimentacoes.scss',
-})
-export class ListagemMovimentacoes {}
