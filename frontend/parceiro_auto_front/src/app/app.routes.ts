91f1f38bb4eb4b4b99a05e50ba4ad87af30da6b3
Adriano Ramos dos Santos Junior <adriannoordsj@gmail.com>
2026-08-15 23:29:54 -0300

    Layout basico finalizado

--- frontend/parceiro_auto_front/src/app/app.routes.ts
+++ frontend/parceiro_auto_front/src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { Layout } from './components/layout/layout';
 import { Dashboard } from './pages/dashboard/dashboard';
+import {FluxoCaixa} from './pages/fluxo-caixa/fluxo-caixa';
+import {Relatorios} from './pages/relatorios/relatorios';
+import {GestaoAcessos} from './pages/gestao-acessos/gestao-acessos';
+import {Conta} from './pages/conta/conta';
 
 export const routes: Routes = [
   {
@@ -8,6 +12,10 @@ export const routes: Routes = [
     component: Layout,
     children: [
       { path: 'dashboard', component: Dashboard },
+      { path: 'conta', component: Conta },
+      { path: 'fluxo-caixa', component: FluxoCaixa },
+      { path: 'relatorios', component: Relatorios },
+      { path: 'gestao-acessos', component: GestaoAcessos },
     ]
   }
 ];
